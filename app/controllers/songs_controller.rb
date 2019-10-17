e6f45acec377a422eed676e8419aef7e737c2596
github username <github email address>
2019-10-16 18:42:47 -0700

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,49 @@
+class SongsController < ApplicationController
+
+    before_action :find_song, only: [:show, :update, :edit, :destroy]
+
+    def index
+        @songs = Song.all
+    end
+
+    def new
+        @song = Song.new
+    end
+
+    def create
+        @song = Song.create(song_params)
+        if @song.valid?
+            redirect_to @song
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @song.update(song_params)
+        if @song.valid?
+            redirect_to @song
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @song.destroy
+        redirect_to @song
+    end
+
+    private
+
+    def song_params
+        params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+    end
+
+    def find_song
+        @song = Song.find(params[:id])
+    end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+first = Song.create(title: "fwebfu", release_year: 2020, artist_name: "HH hvada", genre: "pop")
+
+
+
